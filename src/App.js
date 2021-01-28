4c106c9c6177158367ab866bc317586a85830c8b
elocremarc <42568538+elocremarc@users.noreply.github.com>
2021-01-28 04:58:54 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ render() {
 
     <React.Fragment>  
     <div class="d-flex bg-secondary">
-    <div id="heading" class="m-2 p-2 rounded text-light" ><h1 class="display-6">Web3 Login</h1></div>
+    <div id="heading" class="m-2 p-2 rounded text-light" ><h1 class="display-6">Login</h1></div>
     <div class="flex-grow-1"></div>
 
     <Login/>
